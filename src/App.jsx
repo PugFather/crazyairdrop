2508a0db40c18a3b85dc6b2566988803c439b709
PugFather <pugfathertoken@gmail.com>
2023-12-27 08:24:12 +0100

    wallets

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import {
   CoinbaseWalletAdapter,
   PhantomWalletAdapter,
+  SolflareWalletAdapter,
   TorusWalletAdapter,
+  MathWalletAdapter,
+  TrustWalletAdapter,
+  TokenPocketWalletAdapter,
+  WalletConnectWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
@@ -17,19 +22,24 @@ import { clusterApiUrl } from "@solana/web3.js";
 import("@solana/wallet-adapter-react-ui/styles.css");
 
 export default function WalletAdapter() {
+  const network = WalletAdapterNetwork.Mainnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
   // Add adapters to list of supported wallets
   const wallets = useMemo(
     () => [
       new CoinbaseWalletAdapter(),
       new PhantomWalletAdapter(),
+      new MathWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
       new TorusWalletAdapter(),
+      new TokenPocketWalletAdapter(),
+      new WalletConnectWalletAdapter(),
+      new TrustWalletAdapter(),
     ],
-    []
+    [network],
   );
-
-  const network = WalletAdapterNetwork.Mainnet;
-
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
